011626046293a2a7d1f55da7e14a1cb83672557f
tejus <tejuslohanibrb@gmail.com>
2024-01-21 18:23:34 +0530

    error solved on line no 6 (;)

--- demo1/src/demo/model/Student.java
+++ demo1/src/demo/model/Student.java
@@ -3,7 +3,7 @@ package demo.model;
 public class Student {
 	public static void main(String[] args) {
 		System.out.println("oom namah shivay");
-		System.out.println("changed done on gitHub")
+		System.out.println("changed done on gitHub");
 	}
 
 }
