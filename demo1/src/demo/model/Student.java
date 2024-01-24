87e4bc86ef76d435e7e866228a21a4bbb9440206
tejuslohani <157277283+tejuslohani@users.noreply.github.com>
2024-01-24 14:41:01 +0530

    delete one SOP

--- demo1/src/demo/model/Student.java
+++ demo1/src/demo/model/Student.java
@@ -4,7 +4,7 @@ public class Student {
 	public static void main(String[] args) {
 		System.out.println("oom namah shivay");
 		System.out.println("changed done on gitHub");
-		System.out.println("one more SOP");
+		
 		
 	}
 
